239180621eb7915d210dac122b347e4eebc1cffc
UniversidadAgustiniana <danielag172001@gmail.com>
2024-04-13 13:42:10 -0500

    2

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def uppload_image ():
     request_body = json.loads(request.data)
 
     config = load_config()
-    return os.environ["apk"]
+    return os.environ["TEMP"]
     client = OpenAI(api_key=config['apk'])
     
     response = client.chat.completions.create(
