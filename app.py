383d5bc640fff65f1a6733ec4e8f398d21d4fe59
UniversidadAgustiniana <danielag172001@gmail.com>
2024-04-13 09:45:18 -0500

    commit

--- app.py
+++ app.py
@@ -22,63 +22,53 @@ def get_image_list ():
 @app.route('/upload-image', methods=["POST"])
 def uppload_image ():
     global_imageList=[]
-    try:
-        request_body = json.loads(request.data)        
-        client = OpenAI(api_key=request_body['api_key'])        
+    request_body = json.loads(request.data)        
+    client = OpenAI(api_key=request_body['api_key'])        
 
-        response = client.chat.completions.create(
-          model="gpt-4-vision-preview",
-          messages=[
-            {
-              "role": "user",
-              "content": [
-                {"type": "text", "text": "¿ descripcion de esta imagen contando un chiste colombiano ?"},
-                {
-                  "type": "image_url",
-                  "image_url": {                
-                    "url": f"data:image/jpeg;base64,{request_body['imgBase64']}"
-                  },
-                },
-              ],
-            }
-          ],
-          max_tokens=300,
-        )        
-        
-        imageList = []
-        with open('DB.json') as json_file:        
-            if json_file.read() != '':
-                with open('DB.json', 'r') as json_data:
-                    data = json.load(json_data)
-                    imageList.extend(data)
-                    global_imageList = data
-        
-        
-        dictionary = {
-            "imageBase64": f"{request_body['imgBase64']}",
-            "description": str(response.choices[0].message.content)
-        }        
-        
-        imageList.append(dictionary)
+
     
-        # Serializing json
-        json_object = json.dumps(imageList, indent=4)
-        
-        # Writing to sample.json
-        with open("DB.json", "w") as outfile:
-            outfile.write(json_object)        
-        
-        return response.choices[0].message.content
-      
-    except Exception as e:
-      #Handle API error here, e.g. retry or log
-      # Serializing json
-      json_object = json.dumps(global_imageList, indent=4)
-        
-      # Writing to sample.json
-      with open("DB.json", "w") as outfile:
-            outfile.write(json_object)
-      return f"OpenAI API returned an API Error: {e}"
+    response = client.chat.completions.create(
+      model="gpt-4-vision-preview",
+      messages=[
+        {
+          "role": "user",
+          "content": [
+            {"type": "text", "text": "¿ descripcion de esta imagen contando un chiste colombiano ?"},
+            {
+              "type": "image_url",
+              "image_url": {                
+                "url": f"data:image/jpeg;base64,{request_body['imgBase64']}"
+              },
+            },
+          ],
+        }
+      ],
+      max_tokens=300,
+    )        
+    imageList = []
+    with open('DB.json') as json_file:        
+        if json_file.read() != '':
+            with open('DB.json', 'r') as json_data:
+                data = json.load(json_data)
+                imageList.extend(data)
+                global_imageList = data
+    
+    
+    dictionary = {
+        "imageBase64": f"{request_body['imgBase64']}",
+        "description": str(response.choices[0].message.content)
+    }        
+    
+    imageList.append(dictionary)
+
+    # Serializing json
+    json_object = json.dumps(imageList, indent=4)
+    
+    # Writing to sample.json
+    with open("DB.json", "w") as outfile:
+        outfile.write(json_object)        
+    
+    return response.choices[0].message.content
     
 
 
